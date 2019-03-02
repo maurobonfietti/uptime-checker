22220afab2fd7a23abc09f8773231290e51f0180
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-03-02 03:03:44 -0300

    Re-indent Format.

--- test/unit.js
+++ test/unit.js
@@ -11,23 +11,23 @@ var assert = require('assert');
 var unit = {};
 
 // Logs.list should callback an array and a false error
-unit['logs.list should callback a false error and an array of log names'] = function(done){
-  logs.list(true,function(err,logFileNames){
-      assert.equal(err, false);
-      assert.ok(logFileNames instanceof Array);
-      assert.ok(logFileNames.length > 1);
-      done();
-  });
+unit['logs.list should callback a false error and an array of log names'] = function (done) {
+    logs.list(true, function (err, logFileNames) {
+        assert.equal(err, false);
+        assert.ok(logFileNames instanceof Array);
+        assert.ok(logFileNames.length > 1);
+        done();
+    });
 };
 
 // Logs.truncate should not throw if the logId doesnt exist
-unit['logs.truncate should not throw if the logId does not exist, should callback an error instead'] = function(done){
-  assert.doesNotThrow(function(){
-    logs.truncate('I do not exist',function(err){
-      assert.ok(err);
-      done();
-    })
-  },TypeError);
+unit['logs.truncate should not throw if the logId does not exist, should callback an error instead'] = function (done) {
+    assert.doesNotThrow(function () {
+        logs.truncate('I do not exist', function (err) {
+            assert.ok(err);
+            done();
+        })
+    }, TypeError);
 };
 
 // Export the tests to the runner
